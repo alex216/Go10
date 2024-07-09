eac00def76e88edc955f32aab8b39ace1b89561d
yliu <yliu@XLIJFELKNIX.local>
2024-07-09 15:46:58 +0900

    [fix] Makefile

--- ex10/vendor/piscine/btreedeletenode.go
+++ ex10/vendor/piscine/btreedeletenode.go
@@ -25,27 +25,3 @@ func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
 	BTreeTransplant(root, node, replace)
 	return root
 }
-
-// package piscine
-
-// func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
-// 	if node == nil {
-// 		return root
-// 	}
-// 	if node.Left == nil {
-// 		return BTreeTransplant(root, node, node.Right)
-// 	} else if node.Right == nil {
-// 		return BTreeTransplant(root, node, node.Left)
-// 	} else {
-// 		y := BTreeMin(node.Right)
-// 		if y.Parent != node {
-// 			BTreeTransplant(root, y, y.Right)
-// 			y.Right = node.Right
-// 			y.Right.Parent = y
-// 		}
-// 		BTreeTransplant(root, node, y)
-// 		y.Left = node.Left
-// 		y.Left.Parent = y
-// 		return root
-// 	}
-// }
