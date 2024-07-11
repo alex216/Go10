b2721945aca71154dab5e6859430cc205490428e
yliu <yliu@XLIJFELKNIX.local>
2024-07-11 14:40:57 +0900

    [fix] check parent field

--- ex05/vendor/piscine/btreeisbinary.go
+++ ex05/vendor/piscine/btreeisbinary.go
@@ -1,14 +1,27 @@
 package piscine
 
+func rec(node *TreeNode, parent *TreeNode) bool {
+	if node == nil {
+		return true
+	}
+	if node.Left != nil && node.Left.Data > node.Data {
+		return false
+	}
+	if node.Right != nil && node.Right.Data < node.Data {
+		return false
+	}
+	if node.Parent != parent {
+		return false
+	}
+	return (rec(node.Left, node) && rec(node.Right, node))
+}
+
 func BTreeIsBinary(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	if root.Left != nil && root.Left.Data > root.Data {
+	if root.Parent != nil {
 		return false
 	}
-	if root.Right != nil && root.Right.Data < root.Data {
-		return false
-	}
-	return (BTreeIsBinary(root.Left) && BTreeIsBinary(root.Right))
+	return rec(root, nil)
 }
